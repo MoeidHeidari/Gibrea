3cb3efa21cc52b4070a03089d930642780ce1c04
moeidtopcoder <moeidheidari@mail.ru>
2022-06-24 08:51:20 -0400

    yaml parser test added

--- cmd/root.go
+++ cmd/root.go
@@ -5,37 +5,70 @@ Copyright © 2022 Moeid Heidari
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
+//=================================================================================================================================================================================
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "main",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Gitea batch repo aggregator programm",
+	Long: `                                                        
+	.d8888b.  8888888 888888b.   8888888b.  8888888888        d8888 
+	d88P  Y88b   888   888  "88b  888   Y88b 888              d88888 
+	888    888   888   888  .88P  888    888 888             d88P888 
+	888          888   8888888K.  888   d88P 8888888        d88P 888 
+	888  88888   888   888  "Y88b 8888888P"  888           d88P  888 
+	888    888   888   888    888 888 T88b   888          d88P   888 
+	Y88b  d88P   888   888   d88P 888  T88b  888         d8888888888 
+	 "Y8888P88 8888888 8888888P"  888   T88b 8888888888 d88P     888
+	
+	
+    
+Gibrea is a batch aggregator program that aggregates a bunch of git repositories from different sources and migrate them to a git repository system`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {},
 }
 
+//=================================================================================================================================================================================
+var CmdStart = &cobra.Command{
+	Use:   "start",
+	Short: "starts the gibrea programm",
+	Long:  ``,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+
+		config, _ := cmd.Flags().GetString("config")
+
+		fmt.Println(config)
+
+	},
+}
+
+//=================================================================================================================================================================================
+//=================================================================================================================================================================================
+//=================================================================================================================================================================================
+//=================================================================================================================================================================================
+//=================================================================================================================================================================================
+//=================================================================================================================================================================================
+//=================================================================================================================================================================================
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	rootCmd.AddCommand(CmdStart)
+	CmdStart.PersistentFlags().String("config", "", "name of the user")
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
 }
 
+//=================================================================================================================================================================================
 func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
@@ -48,4 +81,4 @@ func init() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-
+//=================================================================================================================================================================================
--- go.mod
+++ go.mod
@@ -18,4 +18,5 @@ require (
 	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+	golang.org/x/tools v0.1.11 // indirect
 )
