7d01d86c3d6ef04d039176571ff12b07676e5e76
moeidtopcoder <moeidheidari@mail.ru>
2022-06-24 11:20:53 -0400

    commands and arguments finished

--- cmd/root.go
+++ cmd/root.go
@@ -42,10 +42,76 @@ var CmdStart = &cobra.Command{
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
-
 		config, _ := cmd.Flags().GetString("config")
+		assService, _ := cmd.Flags().GetBool("as-service")
+		fmt.Println(config, assService)
 
-		fmt.Println(config)
+	},
+}
+
+//=================================================================================================================================================================================
+var CmdStop = &cobra.Command{
+	Use:   "stop",
+	Short: "stops the program with state persisted",
+	Long:  ``,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("goodbye:(")
+
+	},
+}
+
+//=================================================================================================================================================================================
+var CmdStatus = &cobra.Command{
+	Use:   "status",
+	Short: "shows the current status of the programm",
+	Long:  ``,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("current status")
+
+	},
+}
+
+//=================================================================================================================================================================================
+var CmdPause = &cobra.Command{
+	Use:   "pause",
+	Short: "pauses the prrogramm execution temorarily with last state persisted",
+	Long:  ``,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("program paused")
+
+	},
+}
+
+//=================================================================================================================================================================================
+var CmdLogs = &cobra.Command{
+	Use:   "logs",
+	Short: "shows the the logs of running program",
+	Long:  ``,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+
+		printLogFilePath, _ := cmd.Flags().GetString("print")
+		fmt.Println(printLogFilePath)
+
+	},
+}
+
+//=================================================================================================================================================================================
+var CmdResum = &cobra.Command{
+	Use:   "resum",
+	Short: "reads the last state of the program and resumes the execution",
+	Long:  ``,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("program resumed")
 
 	},
 }
@@ -53,15 +119,18 @@ var CmdStart = &cobra.Command{
 //=================================================================================================================================================================================
 //=================================================================================================================================================================================
 //=================================================================================================================================================================================
-//=================================================================================================================================================================================
-//=================================================================================================================================================================================
-//=================================================================================================================================================================================
-//=================================================================================================================================================================================
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	rootCmd.AddCommand(CmdStart)
-	CmdStart.PersistentFlags().String("config", "", "name of the user")
+	CmdStart.PersistentFlags().String("config", "", "path to the configuration file")
+	CmdStart.PersistentFlags().Bool("as-service", false, "runs the program as a background service")
+	rootCmd.AddCommand(CmdStop)
+	rootCmd.AddCommand(CmdStatus)
+	rootCmd.AddCommand(CmdLogs)
+	CmdLogs.PersistentFlags().String("print", "", "path to the log file you want to print the logs inside")
+	rootCmd.AddCommand(CmdPause)
+	rootCmd.AddCommand(CmdResum)
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
